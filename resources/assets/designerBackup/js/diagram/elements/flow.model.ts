c8bf92c007b00bcf826cc487f7499d59713ea215
Jonas <hjonathan.sge@gmail.com>
2018-04-06 15:04:16 +0000

    Remove folder designerbackup

--- resources/assets/designerBackup/js/diagram/elements/flow.model.ts
+++
@@ -1,7 +0,0 @@
-export interface IFlow {
-    config(options: any);
-    render(): any;
-    getNativeShape(): any
-    redraw(posx1, posy1, posx2, posy2): any
-    renderManhathan(x1, y1, x2, y2): any
-}
