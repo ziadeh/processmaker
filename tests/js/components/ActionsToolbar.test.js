fa51d60b627d76ee9702186e9dda67c7fe499595
Mila Endo <milal.alim@gmail.com>
2018-04-10 17:05:59 -0700

    testing added

---
+++ tests/js/components/ActionsToolbar.test.js
@@ -0,0 +1,30 @@
+import { mount } from '@vue/test-utils'
+import ActionsToolbar from '../../../resources/assets/designer/js/components/ActionsToolbar.vue'
+
+let wrapper = null;
+
+
+describe('ActionsToolbar', () => {
+  beforeEach(() => {
+      wrapper = mount(ActionsToolbar)
+      window.Event = {
+        $emit: jest.fn()
+      }
+  })
+  it('should emit zoomIn when zoomIn button is clicked', () => {
+    wrapper.find('button').trigger('click')
+    expect(window.Event.$emit.mock.calls.length).toBe(1)
+    expect(window.Event.$emit.mock.calls[0][0]).toBe("zoomIn")
+  })
+  it('should emit zoomReset when zoomReset button is clicked', () => {
+    wrapper.find('button').trigger('click')
+    expect(window.Event.$emit.mock.calls.length).toBe(1)
+    expect(window.Event.$emit.mock.calls[0][0]).toBe("zoomReset")
+  })
+  it('should emit zoomOut when zoomOut button is clicked', () => {
+    wrapper.find('button').trigger('click')
+    expect(window.Event.$emit.mock.calls.length).toBe(1)
+    expect(window.Event.$emit.mock.calls[0][0]).toBe("zoomOut")
+  })
+
+})
