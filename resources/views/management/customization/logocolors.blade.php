e9f6037ad9167d04d430da58eed0e5c43840f83c
Leah <leah.miller@processmaker.com>
2018-08-14 15:09:41 -0700

    basic form with color changes prior to vue-color

--- resources/views/management/customization/logocolors.blade.php
+++ resources/views/management/customization/logocolors.blade.php
@@ -9,28 +9,29 @@
     <h4>UI Customization</h4>
     <h5>Add your company logo</h5>
     <form>
-        <label for="companyLogo">{{"companyLogo"}}</label>
-        <!-- <input type="text" id="companyLogo" class="form-control inline-input" v-model="companyLogo"> -->
         <div class="form-group">
             <div class="d-flex">
                 <input type="text" class="form-control inline-input">
                 <button type="submit" class="btn inline-button text-light">Choose</button>
             </div>
-            <small id="addCompanyLogo" class="form-text text-muted">Logo size must be 400x100. File format .jpg or .png</small>
+            <small id="addCompanyLogo" class="text-mute">Logo size must be 400x100. File format .jpg or .png</small>
         </div>
+
         <div class="form-group">
             <div class="d-flex">
                 <input type="text" class="form-control inline-input">
                 <button type="submit" class="btn inline-button text-light">Choose</button>
             </div>
-            <small id="addCompanyLogo" class="form-text text-muted">Logo size must be 400x100. File format .jpg or .png</small>
+            <small id="addCompanyLogo" class="text-mute">Logo size must be 400x100. File format .jpg or .png</small>
         </div>
+
         <h5>Create a color scheme to customize your UI</h5>
         <div class="form-group">
             <label>Enter hex color or chose a color for the left navigation bar</label>
             <input type="text" class="form-control inline-input">
         </div>
-        <div class="form-grpup">
+
+        <div class="form-group">
             <label>Enter hex color or chose a color for the action buttons</label>
             <input type="text" class="form-control inline-input">
         </div>
@@ -59,5 +60,9 @@ export default {
 .inline-button {
   background-color: rgb(109,124,136);
   font-weight: 100;
+}
+
+.text-mute {
+    color: #788793;
 }
 </style>
