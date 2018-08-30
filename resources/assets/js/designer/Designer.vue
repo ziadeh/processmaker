814cdebd5349a0a5f8355d7b53e06b5bcdd8fef3
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2018-08-30 08:07:04 -0400

    fix conflicts

--- resources/assets/js/designer/Designer.vue
+++ resources/assets/js/designer/Designer.vue
@@ -16,49 +16,40 @@
 
 
 <script>
-// Import our designer event-bus
-import EventBus from "./lib/event-bus";
-// Import our top-level components
-// Designer is our overall canvas tool
-import svgcanvas from "./components/svgcanvas";
-// This is our toolbar palette
-import toolbar from "./components/toolbar";
-// This is our top toolbar with process title and process options
-import toptoolbar from "./components/toptoolbar";
-// This is our objects menu with nested object-menu items components
-import designerobjectsmenu from "./components/designer-objects-menu";
-// @todo Figure out a way to add these modals to the properties of components
-import modalCreateDatabaseAdd from "./components/modals/modal-create-database-add";
-import modalCreateOutputAdd from "./components/modals/modal-create-output-add";
-import modalCreateTemplateAdd from "./components/modals/modal-create-template-add";
-import modalCreateScriptAdd from "./components/modals/modal-create-script-add";
-import modalFormsAdd from "./components/modals/modal-forms-add";
-import modalInputDocumentAdd from "./components/modals/modal-input-document-add";
-import modalPermissionsAdd from "./components/modals/modal-permissions-add";
-import modalPublicFileAdd from "./components/modals/modal-public-file-add";
-import modalMessageTypes from "./components/modals/modal-message-types";
-import modalFormsList from "./components/modals/modal-forms-list";
-import ModalScriptTask from "./components/modals/modal-script-task";
-import modalTaskConfiguration from "./components/modals/modal-task-configuration";
-
-//Modal list
-import modalForms from "./components/modalList/modal-forms-list";
-import modalOutputDocuments from "./components/modalList/modal-output-documents";
-import ModalInputDocumentList from "./components/modalList/modal-input-document-list";
-import ModalScriptsList from "./components/modalList/modal-scripts-list";
-
-// This is out Cron for every shape
-import crown from "./components/crown";
-import actions from "./actions";
-
+    // Import our designer event-bus
+    import EventBus from "./lib/event-bus";
+    // Import our top-level components
+    // Designer is our overall canvas tool
+    import svgcanvas from "./components/svgcanvas";
+    // This is our toolbar palette
+    import toolbar from "./components/toolbar";
+    // This is our top toolbar with process title and process options
+    import toptoolbar from "./components/toptoolbar";
+    // This is our objects menu with nested object-menu items components
+    import designerobjectsmenu from "./components/designer-objects-menu";
+    // @todo Figure out a way to add these modals to the properties of components
+    import modalCreateDatabaseAdd from "./components/modals/modal-create-database-add";
+    import modalCreateOutputAdd from "./components/modals/modal-create-output-add";
+    import modalCreateTemplateAdd from "./components/modals/modal-create-template-add";
+    import modalCreateScriptAdd from "./components/modals/modal-create-script-add";
+    import modalFormsAdd from "./components/modals/modal-forms-add";
+    import modalInputDocumentAdd from "./components/modals/modal-input-document-add";
+    import modalPermissionsAdd from "./components/modals/modal-permissions-add";
+    import modalPublicFileAdd from "./components/modals/modal-public-file-add";
+    import modalMessageTypes from "./components/modals/modal-message-types";
+    import modalFormsList from "./components/modals/modal-forms-list";
+    import ModalScriptTask from "./components/modals/modal-script-task";
+    import modalTaskConfiguration from "./components/modals/modal-task-configuration";
+    //Modal list
+    import modalForms from "./components/modalList/modal-forms-list";
+    import modalOutputDocuments from "./components/modalList/modal-output-documents";
+    import ModalInputDocumentList from "./components/modalList/modal-input-document-list";
+    import ModalScriptsList from "./components/modalList/modal-scripts-list";
     // This is out Cron for every shape
-    import crown from "./components/crown"
-    import actions from "./actions"
-
+    import crown from "./components/crown";
+    import actions from "./actions";
     export default {
-        props: [
-            'processUid', 'processTitle'
-        ],
+        props: ["processUid", "processTitle"],
         components: {
             crown,
             designerobjectsmenu,
@@ -71,7 +62,6 @@ import actions from "./actions";
             modalInputDocumentAdd,
             modalPermissionsAdd,
             modalPublicFileAdd,
-            modalVariablesAdd,
             modalOutputDocuments,
             modalFormsList,
             modalTaskConfiguration,
@@ -88,8 +78,8 @@ import actions from "./actions";
                 modalComponent: null,
                 bpmn: {},
                 selectedElement: {},
-                autoSaveTime: 30000 // miliseconds
-            }
+                autoSaveTime: 15000 // miliseconds
+            };
         },
         created() {
             // Listen for opening an add dialog
@@ -103,123 +93,122 @@ import actions from "./actions";
                 // @todo Replace this with dynamic modal generation once we have all modals in place
                 // We're not doing this now so we can have visual alert feedback when a modal isn't implemented
                 switch (key) {
-                    case 'permissions':
-                        this.modalComponent = 'modal-permissions-add'
+                    case "permissions":
+                        this.modalComponent = "modal-permissions-add";
                         break;
-                    case 'variables':
-                        this.modalComponent = 'modal-variables-add'
+                    case "public-files":
+                        this.modalComponent = "modal-public-file-add";
                         break;
-                    case 'public-files':
-                        this.modalComponent = 'modal-public-file-add'
+                    case "forms":
+                        this.modalComponent = "modal-forms-add";
                         break;
-                    case 'forms':
-                        this.modalComponent = 'modal-forms-add'
+                    case "formslist":
+                        this.modalComponent = "modal-forms-list";
                         break;
-                    case 'formslist':
-                        this.modalComponent = 'modal-forms-list'
+                    case "message-types":
+                        this.modalComponent = "modal-message-types";
                         break;
-                    case 'message-types':
-                        this.modalComponent = 'modal-message-types'
+                    case "database-connections":
+                        this.modalComponent = "modal-create-database-add";
                         break;
-                    case 'database-connections':
-                        this.modalComponent = 'modal-create-database-add'
+                    case "input-documents":
+                        this.modalComponent = "modal-input-document-add";
                         break;
-                    case 'input-documents':
-                        this.modalComponent = 'modal-input-document-add'
+                    case "output-documents":
+                        this.modalComponent = "modal-create-output-add";
                         break;
-                    case 'output-documents':
-                        this.modalComponent = 'modal-create-output-add'
+                    case "scripts":
+                        this.modalComponent = "modal-create-script-add";
                         break;
-                    case 'scripts':
-                        this.modalComponent = 'modal-create-script-add'
+                    case "templates":
+                        this.modalComponent = "modal-create-template-add";
                         break;
-                    case 'templates':
-                        this.modalComponent = 'modal-create-template-add'
+                    case "open":
+                        this.modalComponent = "modal-create-template-add";
                         break;
-                    case 'open':
-                        this.modalComponent = 'modal-create-template-add'
-                        break;
-                    case 'show-element-configuration':
+                    case "show-element-configuration":
                         switch (this.selectedElement.type) {
-                          case 'scripttask':
-                            this.modalComponent = 'modal-script-task'
-                            break;
+                            case "scripttask":
+                                this.modalComponent = "modal-script-task";
+                                break;
                         }
-                    case 'task-configuration':
-                        this.modalComponent = 'modal-task-configuration'
+                    case "task-configuration":
+                        this.modalComponent = "modal-task-configuration";
                         break;
                     default:
-                        alert(key + ' add modal not yet implemented.')
+                        alert(key + " add modal not yet implemented.");
                 }
             },
-            openTitleDialog(key){
+            openTitleDialog(key) {
                 switch (key) {
-                    case 'forms':
-                        this.modalComponent = 'modal-forms';
+                    case "forms":
+                        this.modalComponent = "modal-forms";
                         break;
-                    case 'input-documents':
-                        this.modalComponent = 'modal-input-document-list';
+                    case "input-documents":
+                        this.modalComponent = "modal-input-document-list";
                         break;
-                    case 'output-documents':
-                        this.modalComponent = 'modal-output-documents';
+                    case "output-documents":
+                        this.modalComponent = "modal-output-documents";
                         break;
-                    case 'scripts':
-                        this.modalComponent = 'modal-scripts-list';
+                    case "scripts":
+                        this.modalComponent = "modal-scripts-list";
                         break;
                     default:
-                        alert(key + ' Behavior TBD')
+                        alert(key + " Behavior TBD");
                 }
             },
-            onHidden(){
-                this.modalComponent = null
+            onHidden() {
+                this.modalComponent = null;
             },
-            onScroll(){
-                let action = actions.designer.crown.hide()
-                EventBus.$emit(action.type, action.payload)
+            onScroll() {
+                let action = actions.designer.crown.hide();
+                EventBus.$emit(action.type, action.payload);
             },
-            autoSave(){
+            autoSave() {
                 setInterval(() => {
-                    let action = actions.bpmn.save()
-                    EventBus.$emit(action.type, action.payload)
-                }, this.autoSaveTime)
+                    let action = actions.bpmn.save();
+                    EventBus.$emit(action.type, action.payload);
+                }, this.autoSaveTime);
             }
         },
-        mounted(){
-            ProcessMaker.apiClient.get(`processes/${this.processUid}/bpmn`, {
-                params: {}
-            }).then((response) => {
-                let action = actions.designer.bpmn.update(response.data)
-                EventBus.$emit(action.type, action.payload)
-            })
-            this.autoSave()
+        mounted() {
+            ProcessMaker.apiClient
+                .get(`processes/${this.processUid}/bpmn`, {
+                    params: {}
+                })
+                .then(response => {
+                    let action = actions.designer.bpmn.update(response.data);
+                    EventBus.$emit(action.type, action.payload);
+                });
+            this.autoSave();
         }
     };
 </script>
 
 <style lang="scss" scoped>
-#designer-container {
-  // Width and height is set to 100% to fill the outside container
-  max-height: 100%;
-  min-height: 100%;
-  max-width: 100%;
-  min-width: 100%;
-  flex: 1;
-  // We will flex container the items
-  display: flex;
-  flex-direction: column;
-  background-color: green;
-  #designer-subcontainer {
-    display: flex;
-    flex: 1;
-    flex-direction: row;
-    min-width: 100%;
-    max-width: 100%;
-    min-height: 100%;
-    max-height: 100%;
-    .canvas-container {
-      flex: 1;
-      overflow: auto;
+    #designer-container {
+        // Width and height is set to 100% to fill the outside container
+        max-height: 100%;
+        min-height: 100%;
+        max-width: 100%;
+        min-width: 100%;
+        flex: 1;
+        // We will flex container the items
+        display: flex;
+        flex-direction: column;
+        background-color: green;
+        #designer-subcontainer {
+            display: flex;
+            flex: 1;
+            flex-direction: row;
+            min-width: 100%;
+            max-width: 100%;
+            min-height: 100%;
+            max-height: 100%;
+            .canvas-container {
+                flex: 1;
+                overflow: auto;
+            }
+        }
     }
-  }
-}
 </style>
