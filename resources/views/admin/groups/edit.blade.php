303129506ccbcb66a32363f7160b08d09ec193eb
Leah <leah.miller@processmaker.com>
2018-10-12 15:58:43 -0700

    corrected content

--- resources/views/admin/groups/edit.blade.php
+++ resources/views/admin/groups/edit.blade.php
@@ -4,48 +4,80 @@
   {{__('Edit Groups')}}
 @endsection
 
-@Section('sidebar')
+@section('sidebar')
 @include('layouts.sidebar', ['sidebar'=> Menu::get('sidebar_admin')])
 @endsection
 
-@Section('content')
+@section('content')
+
 <div class="container">
-      <h1 style="margin:15px">Edit Group</h1>
-      <div class="row">
-        <div class="col-8">
-          <div class="card card-body">
-            <form>
-              <div class="form-group">
-                <label for="groupName">Group Name</label>
-                <input type="text" class="form-control" id="groupName" value="{{$group->name}}">
-              </div>
-              <div class="form-group">
-                <label for="description">Description</label>
-                <textarea class="form-control" id="description" rows="3"></textarea>
-              </div>
-              <div class="form-group p-0">
-                <label for="dropdownSelect">Status</label>
-                <select class="form-control" id="dropdownSelect">
-                  <option>Active</option>
-                  <option>Inactive</option>
-                  <option>Draft</option>
-                </select>
-              </div>
-            </form>
-            <div class="card-body text-right pr-0">
-              <button type="button" class="btn btn-outline-success" data-dismiss="modal">Cancel</button>
-              <button type="submit" class="btn btn-success ml-2">Save</button>
-            </div>
-          </div>
+  <h1 style="margin:15px">Edit Group</h1>
+  <div class="row">
+    <div class="col-8">
+      <div class="card card-body">
+      {!! Form::model($group) !!}
+        <div class="form-group">
+          {!! Form::label('name', 'Group Name')!!}
+          {!! Form::text('name', null, ['class'=> 'form-control']) !!}
+          <div class="text-danger" v-if="addError.title">@{{addError.title[0]}}</div>
         </div>
-        <div class="col-4">
-          <div class="card card-body">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <div class="form-group">
+          {!! Form::label('description', 'Description') !!}
+          {!! Form::textarea('description', null, ['class'=> 'form-control', 'rows' => 3]) !!}
+          <div class="text-danger" v-if="addError.description">@{{addError.title[0]}}</div>
         </div>
+        <div class="form-group p-0">
+          {!! Form::label('status', 'Status'); !!}
+          {!! Form::select('status', ['Active', 'Inactive', 'Draft'], null, ['class' => 'form-control']) !!}
         </div>
+        
+        <div class="card-body text-right pr-0">
+          {!! Form::button('Cancel', ['class'=>'btn btn-outline-success']) !!}
+          {!! Form::submit('Save', ['class'=>'btn btn-success ml-2']) !!}
+        </div>
+        {!! Form::close() !!}
       </div>
     </div>
+    <div class="col-4">
+      <div class="card card-body">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+  </div>
+</div>
 @endsection
 
-@Section('js')
+@section('js')
+<script>
+  new Vue({
+    el: '#addScript',
+    data: {
+      name: '',
+      description: '',
+      status: '',
+      addError: {},
+      submitted: false
+    },
+    methods: {
+      onSubmit() {
+        this.submitted = true;
+        ProcessMaker.apiClient.put("/groups/" + uuid, {
+          name: this.name,
+          description: this.description
+        })
+        .then(response => {
+          ProcessMaker.alert('Group successfully updated', 'success')
+          window.location = "/admin/groups/" + response.data.uuid
+        })
+        .catch(error => {
+          if (error.response.status === 422) {
+            this.addError = error.response.data.errors
+          }
+        })
+        .finally(()=> {
+          this.submitted = false
+        })
+      }
+    }
+  })       
+</script>
 @endsection
