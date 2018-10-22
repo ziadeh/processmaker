2eab405750c2c8a8c5d52204d1c27d42cd80402a
Mila Endo <milal.alim@gmail.com>
2018-10-22 12:09:34 -0700

    testing edit route

--- tests/Feature/Processes/EnvironmentVariablesTest.php
+++ tests/Feature/Processes/EnvironmentVariablesTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Processes;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\Feature\Shared\RequestHelper;
+use ProcessMaker\Models\EnvironmentVariable;
 
 class EnvironmentVariablesTest extends TestCase
 {
@@ -26,5 +27,23 @@ class EnvironmentVariablesTest extends TestCase
       $response->assertStatus(200);
 
       $response->assertSee('Environment Variables');
+    }
+
+      /**
+     * Test to make sure the controller and route work with the view
+     *
+     * @return void
+     */
+    public function testEditRoute()
+    {
+
+        $envvar_uuid = factory(EnvironmentVariable::class)->create()->uuid_text;
+        // get the URL
+        $response = $this->webCall('GET', '/processes/environment-variables/' . $envvar_uuid . '/edit');
+
+        $response->assertStatus(200);
+        // check the correct view is called
+        $response->assertViewIs('processes.environment-variables.edit');
+        // $response->assertSee('Edit Environment Variables');
     }
 }
