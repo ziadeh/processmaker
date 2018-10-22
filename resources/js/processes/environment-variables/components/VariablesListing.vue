7af916b3b0fbb5704d6a3f95694361161b2fc352
Mila Endo <milal.alim@gmail.com>
2018-10-22 10:32:23 -0700

    wired up functionality for edit action button

--- resources/js/processes/environment-variables/components/VariablesListing.vue
+++ resources/js/processes/environment-variables/components/VariablesListing.vue
@@ -1,22 +1,21 @@
 <template>
-    <div class="data-table">
-        <vuetable :dataManager="dataManager" :sortOrder="sortOrder" :css="css" :api-mode="false"
-                  @vuetable:pagination-data="onPaginationData" :fields="fields" :data="data" data-path="data"
-                  pagination-path="meta">
-            <template slot="actions" slot-scope="props">
-                <div class="actions">
-                    <div class="popout">
-                        <b-btn variant="action" @click="onAction('edit-item', props.rowData, props.rowIndex)"
-                               v-b-tooltip.hover title="Edit"><i class="fas fa-edit"></i></b-btn>
-                        <b-btn variant="action" @click="onAction('remove-item', props.rowData, props.rowIndex)"
-                               v-b-tooltip.hover title="Remove"><i class="fas fa-trash-alt"></i></b-btn>
-                    </div>
-                </div>
-            </template>
-        </vuetable>
-        <pagination single="Variable" plural="Variables" :perPageSelectEnabled="true" @changePerPage="changePerPage"
-                    @vuetable-pagination:change-page="onPageChange" ref="pagination"></pagination>
-    </div>
+  <div class="data-table">
+    <vuetable :dataManager="dataManager" :sortOrder="sortOrder" :css="css" :api-mode="false" @vuetable:pagination-data="onPaginationData"
+      :fields="fields" :data="data" data-path="data" pagination-path="meta">
+      <template slot="actions" slot-scope="props">
+        <div class="actions">
+          <div class="popout">
+            <b-btn variant="action" @click="onAction('edit-item', props.rowData, props.rowIndex)" v-b-tooltip.hover
+              title="Edit"><i class="fas fa-edit"></i></b-btn>
+            <b-btn variant="action" @click="onAction('remove-item', props.rowData, props.rowIndex)" v-b-tooltip.hover
+              title="Remove"><i class="fas fa-trash-alt"></i></b-btn>
+          </div>
+        </div>
+      </template>
+    </vuetable>
+    <pagination single="Variable" plural="Variables" :perPageSelectEnabled="true" @changePerPage="changePerPage"
+      @vuetable-pagination:change-page="onPageChange" ref="pagination"></pagination>
+  </div>
 </template>
 
 <script>
@@ -67,9 +66,13 @@ export default {
     };
   },
   methods: {
+    goToEdit(data) {
+      window.location = "/processes/environment-variables/" + data + "/edit";
+    },
     onAction(action, data, index) {
       switch (action) {
         case "edit-item":
+          this.goToEdit(data.uuid);
           break;
         case "remove-item":
           break;
