5e64c82ac7411975a283db1003791f381b37f431
Roberto Gongora <roberto.gongora@processmaker.com>
2018-12-05 19:03:00 -0400

    add test case related to users pagination

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Browser;
 
+use Illuminate\Support\Facades\Artisan;
+use ProcessMaker\Models\User;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 
@@ -12,9 +14,33 @@ class LoginTest extends DuskTestCase
      */
     public function testLogin()
     {
+        Artisan::call('migrate:fresh', []);
+        $user = factory(User::class)->create([
+            'username' => 'admin',
+            'password' => 'admin',
+            'email' => 'any@gmail.com',
+            'firstname' => 'admin',
+            'lastname' => 'admin',
+            'timezone' => null,
+            'datetime_format' => null,
+            'status' => 'ACTIVE',
+            'is_administrator' => true,
+        ]);
+        factory(User::class, 99)->create();
+        // Test login
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
-                    ->assertSee('Username');
+                ->assertSee('Username')
+                ->type('#username', 'admin')
+                ->type('#password', 'admin')
+                ->press('.btn')
+                ->clickLink('Admin')
+                ->pause(5000)
+                ->waitFor('.vuetable-body', 5)
+                ->assertSee('1 - 10 of 100 Users')
+                ->click('div.icon:nth-child(8)')
+                ->pause(1000)
+                ->assertSee('11 - 20 of 100 Users');
         });
     }
 }
