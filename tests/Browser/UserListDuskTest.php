21d0d3440ad777aca66dc35de624473bd843d844
Benjamin Fields <benjamin.fields@processmaker.com>
2019-01-22 12:55:44 -0800

    Trying a new method of making this test work.

--- tests/Browser/UserListDuskTest.php
+++ tests/Browser/UserListDuskTest.php
@@ -34,11 +34,12 @@ class loginTest extends DuskTestCase
         ]);
         factory(User::class, 100)->create();
         $this->browse(function ($browser) {
-            $browser->visit("https://bpm4.local.processmaker.com/admin/users")
+            $browser->visit("/")
                     ->type("#username","admin")
                     ->type("#password","admin")
                     ->click(".btn-success")
-                    ->assertMissing(".invalid-feedback")
+                    ->assertMissing(".invalid-feedback");
+            $browser->visit("/admin/users")
                     ->waitUntilMissing('.vuetable-empty-result');
             $browser->assertSeeIn("#users-listing", "1 - 10 of 101 Users")
                     ->click(".fa-angle-right")
