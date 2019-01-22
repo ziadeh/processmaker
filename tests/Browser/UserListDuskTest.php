da015a38bf806ef41a337e793b57c12a3fff31f3
Benjamin Fields <benjamin.fields@processmaker.com>
2019-01-22 12:37:32 -0800

    Straight up stole some code from UserCreationTest.php to ensure the admin user gets created. Added a new waitUntilMissing() command because now with 101 users, the vuetable doesn't populate quickly. Default wait time is 5 seconds.

--- tests/Browser/UserListDuskTest.php
+++ tests/Browser/UserListDuskTest.php
@@ -5,6 +5,8 @@ namespace Tests\Browser;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use ProcessMaker\Models\User;
@@ -18,13 +20,26 @@ class loginTest extends DuskTestCase
      */
     public function testExample()
     {
+        Artisan::call('migrate:fresh', []);
+        $user = factory(User::class)->create([
+            'username' => 'admin',
+            'password' => Hash::make('admin'),
+            'email' => 'any@gmail.com',
+            'firstname' => 'admin',
+            'lastname' => 'admin',
+            'timezone' => null,
+            'datetime_format' => null,
+            'status' => 'ACTIVE',
+            'is_administrator' => true,
+        ]);
         factory(User::class, 100)->create();
         $this->browse(function ($browser) {
             $browser->visit("https://bpm4.local.processmaker.com/admin/users")
                     ->type("#username","admin")
                     ->type("#password","admin")
                     ->click(".btn-success")
-                    ->assertMissing(".invalid-feedback");
+                    ->assertMissing(".invalid-feedback")
+                    ->waitUntilMissing('.vuetable-empty-result');
             $browser->assertSeeIn("#users-listing", "1 - 10 of 101 Users")
                     ->click(".fa-angle-right")
                     ->pause(750)
