72daf6dd9a878c24d06f060db7ef1b624a3b9906
Benjamin Fields <benjamin.fields@processmaker.com>
2019-03-28 11:28:11 -0700

    This headache is potentially over. Fixed the "use" parts so Artisan runs properly, and the Factory also runs. Added an extra waitForText command to ensure the text is correct, and removed the 800ms pause for a "waitFor" command to make the test run more efficiently.

--- tests/Browser/EnvironmentVariablesTest.php
+++ tests/Browser/EnvironmentVariablesTest.php
@@ -5,7 +5,8 @@ namespace Tests\Browser;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
+use ProcessMaker\Models\User;
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Artisan;
@@ -17,7 +18,7 @@ class EnvironmentVariablesCreationTest extends DuskTestCase
      */
     public function testEnvironmentVariablesCreation()
     {
-        //         $this->markTestSkipped('Skipping Dusk tests temporarily');
+        //$this->markTestSkipped('Skipping Dusk tests temporarily');
 
         //Create Admin User
         Artisan::call('migrate:fresh', []);
@@ -46,7 +47,7 @@ class EnvironmentVariablesCreationTest extends DuskTestCase
             //Add Environment Variable
             $browser->driver->findElement(WebDriverBy::xpath("//*[@id='process-variables-listing']/div[1]/div[2]/button"))
                 ->click();  //The add button lacks a unique ID
-            //                ->press(".btn-secondary") //We can use this line and remove the previous two once the add button is updated
+            //    ->press(".btn-secondary") //We can use this line and remove the previous two once the add button is updated
             $browser->type("#name", "foobar")
                 ->type("#description", "Bars of Foo.")
                 ->type("#value", "foobar")
@@ -66,8 +67,9 @@ class EnvironmentVariablesCreationTest extends DuskTestCase
             //Delete Environment Variable
             $browser->press(".fa-trash-alt")
                 ->waitFor('.modal-content', 10)
+                ->waitForText('Are you sure you want to delete the environment variable barfoo?', 10)
                 ->press("#confirm")
-                ->pause(800)   //No choice here, we have to pause for either the error message or the success alert.
+                ->waitFor(".vuetable-empty-result")
                 ->assertDontSee("barfoo");
         });
     }
