7fe00ebd7b461f978c8f29994838a38bd9ae3b35
Benjamin Fields <benjamin.fields@processmaker.com>
2019-07-17 12:19:49 -0700

    Modified ImportProcess.php: * Realized ImportProcess.php was too complex and relied on outside data from a third party that cannot be guaranteed to always be present. * Created new simple Process (dusk_test.json) that has a basic one button Screen and a self-contained PHP script. * Updated ImportProcess.php to use the new Process, and now runs both a Process import and a full Process run. * Deleted qa_test-start_event.json

--- tests/Browser/ImportProcess.php
+++ tests/Browser/ImportProcess.php
@@ -68,13 +68,13 @@ class ImportProcessTestCase extends DuskTestCase
         $this->browse(function ($browser) {
             $browser->press('.fa-file-import')
                 ->assertVisible('.fa-exclamation-circle')
-                ->attach('input.d-none','tests/Browser/qa_test-start_event.json')
+                ->attach('input.d-none','tests/Browser/dusk_test.json')
                 ->press('#card-footer-pre-import > button.btn.btn-secondary.ml-2') //Import Button
                 ->waitFor('.alert-success',20)
                 ->press('.close') //Close Alert Banner
                 ->assertSee('Successfully imported Process Category')
                 ->assertSee('Successfully imported Process')
-                ->assertSee('Successfully imported Process')
+                ->assertSee('Successfully imported Scripts')
                 ->assertSee('Successfully imported Screens');
         });
     }
@@ -89,51 +89,37 @@ class ImportProcessTestCase extends DuskTestCase
                 ->click('#assignable-table > tbody > tr:nth-child(1) > td.assignable-entity > div > div.multiselect__content-wrapper > ul > li:nth-child(2) > span > span');
             $browser->pause(200);
 
-            //Assign task Choose Your Path
+            //Assign task Click Button
             $browser->click('#assignable-table > tbody > tr:nth-child(2) > td.assignable-entity > div > div.multiselect__select')
                 ->waitForText('Special Assignments',5)
                 ->click('#assignable-table > tbody > tr:nth-child(2) > td.assignable-entity > div > div.multiselect__content-wrapper > ul > li:nth-child(5) > span > span');
             $browser->pause(200);
 
-            //Assign task Submit Numbers
-            $browser->click('#assignable-table > tbody > tr:nth-child(3) > td.assignable-entity > div > div.multiselect__select')
-                ->waitForText('Special Assignments',5)
-                ->click('#assignable-table > tbody > tr:nth-child(3) > td.assignable-entity > div > div.multiselect__content-wrapper > ul > li:nth-child(5) > span > span');
-            $browser->pause(200);
-
-            //Run script Timing and Scoring
-            $browser->click('#assignable-table > tbody > tr:nth-child(4) > td.assignable-entity > div > div.multiselect__select');
+            //Run script RunScript
+            $browser->click('#assignable-table > tbody > tr:nth-child(3) > td.assignable-entity > div > div.multiselect__select');
             $browser->pause(500)
                 ->waitForText('admin admin',5);
-            $browser->click('#assignable-table > tbody > tr:nth-child(4) > td.assignable-entity > div > div.multiselect__content-wrapper > ul > li.multiselect__element > span > span');
-            $browser->pause(200);
-
-            //Run script Compound Interest Calculator - LUA
-            $browser->click('#assignable-table > tbody > tr:nth-child(5) > td.assignable-entity > div > div.multiselect__select')
-                ->waitForText('admin admin',5);
-            $browser->click('#assignable-table > tbody > tr:nth-child(5) > td.assignable-entity > div > div.multiselect__content-wrapper > ul > li.multiselect__element > span > span');
+            $browser->click('#assignable-table > tbody > tr:nth-child(3) > td.assignable-entity > div > div.multiselect__content-wrapper > ul > li.multiselect__element > span > span');
             $browser->pause(200);
 
             //Assign Cancel Request
-            $browser->click('#assignable-table > tbody > tr:nth-child(6) > td.assignable-entity > div > div.multiselect__select')
+            $browser->click('#assignable-table > tbody > tr:nth-child(4) > td.assignable-entity > div > div.multiselect__select')
                 ->waitForText('Users',5);
-            $browser->click('#assignable-table > tbody > tr:nth-child(6) > td.assignable-entity > div > div.multiselect__content-wrapper > ul > li:nth-child(5) > span > span');
+            $browser->click('#assignable-table > tbody > tr:nth-child(4) > td.assignable-entity > div > div.multiselect__content-wrapper > ul > li:nth-child(5) > span > span');
             $browser->pause(200);
 
             //Assign Edit Data
-            $browser->click('#assignable-table > tbody > tr:nth-child(7) > td.assignable-entity > div > div.multiselect__select')
+            $browser->click('#assignable-table > tbody > tr:nth-child(5) > td.assignable-entity > div > div.multiselect__select')
                 ->waitForText('Users',5);
-            $browser->click('#assignable-table > tbody > tr:nth-child(7) > td.assignable-entity > div > div.multiselect__content-wrapper > ul > li:nth-child(5) > span > span');
+            $browser->click('#assignable-table > tbody > tr:nth-child(5) > td.assignable-entity > div > div.multiselect__content-wrapper > ul > li:nth-child(5) > span > span');
 
             //Assert Choices & Save
             $browser
                 ->assertSeeIn('#assignable-table > tbody > tr:nth-child(1) > td.assignable-entity > div > div.multiselect__tags > span', 'admin admin')
                 ->assertSeeIn('#assignable-table > tbody > tr:nth-child(2) > td.assignable-entity > div > div.multiselect__tags > span', 'admin admin')
                 ->assertSeeIn('#assignable-table > tbody > tr:nth-child(3) > td.assignable-entity > div > div.multiselect__tags > span', 'admin admin')
-                ->assertSeeIn('#assignable-table > tbody > tr:nth-child(4) > td.assignable-entity > div > div.multiselect__tags > span', 'admin admin')
-                ->assertSeeIn('#assignable-table > tbody > tr:nth-child(5) > td.assignable-entity > div > div.multiselect__tags > span', 'admin admin')
-                ->assertSeeIn('#assignable-table > tbody > tr:nth-child(6) > td.assignable-entity > div > div.multiselect__tags > div.multiselect__tags-wrap > span > span', 'admin admin')
-                ->assertSeeIn('#assignable-table > tbody > tr:nth-child(7) > td.assignable-entity > div > div.multiselect__tags > div.multiselect__tags-wrap > span > span', 'admin admin')
+                ->assertSeeIn('#assignable-table > tbody > tr:nth-child(4) > td.assignable-entity > div > div.multiselect__tags > div.multiselect__tags-wrap > span > span', 'admin admin')
+                ->assertSeeIn('#assignable-table > tbody > tr:nth-child(5) > td.assignable-entity > div > div.multiselect__tags > div.multiselect__tags-wrap > span > span', 'admin admin')
                 ->click('#card-footer-post-import > div > button'); //Save Button
         });
     }
@@ -142,9 +128,59 @@ class ImportProcessTestCase extends DuskTestCase
     {
         //$this->markTestSkipped('Skipping Dusk tests temporarily');
         $this->browse(function ($browser) {
-            $browser->waitFor('.vuetable-body',5)
-                ->waitUntilMissing('.vuetable-empty-result',5)
-                ->assertSee('QA Test-Start Event');
+            $browser->waitFor('.vuetable-body',10)
+                ->waitUntilMissing('.vuetable-empty-result',10)
+                ->assertSee('DuskTest');
+        });
+    }
+
+    public function testStartProcess()
+    {
+        //$this->markTestSkipped('Skipping Dusk tests temporarily');
+        $this->browse(function ($browser) {
+            $browser->click('#navbar-request-button')
+                ->waitFor('.process-list',10)
+                ->assertSee('DuskTest')
+                ->click('.fa-caret-square-right')
+                ->waitFor('.lds-gear',10)
+                ->waitUntilMissing('.lds-gear',10)
+                ->assertSee('DuskTest')
+                ->assertSeeIn('.bg-success','1');  //In Progress
+        });
+    }
+
+    public function testRunProcess()
+    {
+        //$this->markTestSkipped('Skipping Dusk tests temporarily');
+        $this->browse(function ($browser) {
+            $browser->click('.fa-caret-square-right')
+                ->waitUntilMissing('.vuetable-empty-result',10)
+                ->waitFor('.vuetable-slot',10)
+                //Click Button
+                ->assertSeeIn('#pending > div > div > table > tbody > tr > td:nth-child(1)','Click Button')
+                ->clickLink('Click Button')
+                ->waitFor('.form-group',10)
+                ->assertVisible('.btn-dark')
+                ->click('.btn-dark');
+        });
+    }
+
+    public function testVerifyProcessFinished()
+    {
+        //$this->markTestSkipped('Skipping Dusk tests temporarily');
+        $this->browse(function ($browser) {
+            $browser->waitFor('.lds-gear',10)
+                ->waitUntilMissing('.lds-gear',10)
+                ->assertSee('You don\'t currently have any tasks assigned to you')
+                ->clickLink('Requests')
+                ->waitUntilMissing('.lds-gear',10)
+                ->assertSee('No Results')
+                ->assertSeeIn('.bg-primary','1')  //Completed
+                ->clickLink('Completed')
+                ->waitUntilMissing('.lds-gear',10)
+                ->assertSee('DuskTest')
+                ->click('.fa-caret-square-right')
+                ->assertSeeIn('#summary > table > tbody > tr:nth-child(2)','47');
         });
     }
 }
