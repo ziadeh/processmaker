ef024f4030e13293e4489c25c02983efba1d9665
David Callizaya <davidcallizaya@gmail.com>
2019-08-09 16:38:46 -0400

    Performace test for models

--- database/factories/ProcessVersionFactory.php
+++ database/factories/ProcessVersionFactory.php
@@ -4,16 +4,21 @@ use Faker\Generator as Faker;
 use ProcessMaker\Models\ProcessVersion;
 use ProcessMaker\Models\Process;
 use ProcessMaker\Models\ProcessCategory;
+use ProcessMaker\Models\User;
 
 /**
  * Model factory for a ProcessVersion
  */
 $factory->define(ProcessVersion::class, function (Faker $faker) {
-    $emptyProcess = $faker->file(Process::getProcessTemplatesPath());
+    $emptyProcess = array_random(glob(Process::getProcessTemplatesPath() . '/*.bpmn'));
     return [
         'bpmn' => file_get_contents($emptyProcess),
         'name' => $faker->sentence(3),
         'status' => $faker->randomElement(['ACTIVE', 'INACTIVE']),
+        'user_id' => function () {
+            return factory(User::class)->create()->getKey();
+        },
+        'description' => $faker->sentence,
         'process_category_id' => function () {
             return factory(ProcessCategory::class)->create()->getKey();
         },
---
+++ tests/Feature/Api/Performance.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use ProcessMaker\Models\Group;
+use ProcessMaker\Models\Process;
+use Tests\Feature\Shared\ResourceAssertionsTrait;
+use Tests\TestCase;
+use Tests\Feature\Shared\RequestHelper;
+use Illuminate\Database\Eloquent\Factory as EloquentFactory;
+use ReflectionObject;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Tests routes related to processes / CRUD related methods
+ *
+ * @group process_tests
+ */
+class ProcessTest extends TestCase
+{
+    use WithFaker;
+    use RequestHelper;
+    use ResourceAssertionsTrait;
+
+    // Speed of model Group creation (records/unit_time):
+    // u=71.48 σ=22.16 => Min Speed of distribution = 27.16
+    // Maximum allowed payload per creation: 2 times the creation
+    const MIN_SPEED = 27 / (1 + 2 * 71 / 27);
+
+    private $exceptions = [
+        // Hi payload because of password hash
+        //'ProcessMaker\Models\User' => 27*6/100,
+    ];
+
+    /**
+     * List the factories
+     *
+     * @return array
+     */
+    public function CaseProvider()
+    {
+        $factories = app(EloquentFactory::class);
+        $reflection = new ReflectionObject($factories);
+        $property = $reflection->getProperty('definitions');
+        $property->setAccessible(true);
+        $definitions = $property->getValue($factories);
+
+        $model = 'ProcessMaker\Models\Group';
+        $t = microtime(true);
+        factory($model, 100)->create();
+        $baseTime = microtime(true) - $t;
+        $model::getQuery()->delete();
+
+        $models = [];
+        foreach ($definitions as $model => $definition) {
+            //if ($model==='ProcessMaker\Models\Notification') {
+            $models[] = [$model, $baseTime];
+            //}
+        }
+        return $models;
+    }
+
+    /**
+     *
+     *
+     * @param [type] $model
+     * @param [type] $baseCount
+     * @param [type] $baseTime
+     *
+     * @dataProvider CaseProvider
+     */
+    public function testFactories($model, $baseTime)
+    {
+        $baseCount = $this->getTotalRecords();
+        $t = microtime(true);
+        factory($model)->create();
+        $time = microtime(true) - $t;
+        $count = $this->getTotalRecords();
+        $speed = ($count - $baseCount) / ($time / $baseTime);
+
+        $minSpeed = isset($this->exceptions[$model]) ? $this->exceptions[$model] : self::MIN_SPEED;
+        //error_log($speed / $minSpeed);
+        $this->assertGreaterThanOrEqual($minSpeed, $speed);
+    }
+
+    /**
+     * Get total count of records in the databases
+     *
+     * @return int
+     */
+    private function getTotalRecords()
+    {
+        $tables = [];
+        foreach (config('database.connections') as $name => $config) {
+            $connection = DB::connection($name);
+            $list = $connection->getDoctrineSchemaManager()->listTableNames();
+            foreach ($list as $table) {
+                if (!isset($tables[$table])) {
+                    $tables[$table] = $connection->table($table)->count();
+                }
+            }
+        }
+        return array_sum($tables);
+    }
+}
