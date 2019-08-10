2a302a4a9124803fbce3788cf89b7f5857d0a9b8
David Callizaya <davidcallizaya@gmail.com>
2019-08-10 00:30:28 -0400

    Tests routes speed

--- tests/Feature/Api/Performance.php
+++ tests/Feature/Api/Performance.php
@@ -11,6 +11,9 @@ use Tests\Feature\Shared\RequestHelper;
 use Illuminate\Database\Eloquent\Factory as EloquentFactory;
 use ReflectionObject;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+use ProcessMaker\Models\User;
+use ProcessMaker\Models\Comment;
 
 /**
  * Tests routes related to processes / CRUD related methods
@@ -38,7 +41,7 @@ class ProcessTest extends TestCase
      *
      * @return array
      */
-    public function CaseProvider()
+    public function FactoryListProvider()
     {
         $factories = app(EloquentFactory::class);
         $reflection = new ReflectionObject($factories);
@@ -46,11 +49,7 @@ class ProcessTest extends TestCase
         $property->setAccessible(true);
         $definitions = $property->getValue($factories);
 
-        $model = 'ProcessMaker\Models\Group';
-        $t = microtime(true);
-        factory($model, 100)->create();
-        $baseTime = microtime(true) - $t;
-        $model::getQuery()->delete();
+        $baseTime = $this->calculateUnitTime();
 
         $models = [];
         foreach ($definitions as $model => $definition) {
@@ -59,6 +58,23 @@ class ProcessTest extends TestCase
             //}
         }
         return $models;
+    }
+
+    /**
+     * Time unit base for the performce tests
+     *
+     * @param integer $times
+     *
+     * @return float
+     */
+    private function calculateUnitTime($times = 100)
+    {
+        $model = Group::class;
+        $t = microtime(true);
+        factory($model, $times)->create();
+        $baseTime = microtime(true) - $t;
+        $model::getQuery()->delete();
+        return $baseTime;
     }
 
     /**
@@ -80,7 +96,7 @@ class ProcessTest extends TestCase
         $speed = ($count - $baseCount) / ($time / $baseTime);
 
         $minSpeed = isset($this->exceptions[$model]) ? $this->exceptions[$model] : self::MIN_SPEED;
-        error_log('['.$speed / $minSpeed.']');
+        error_log('[' . $speed / $minSpeed . ']');
         $this->assertGreaterThanOrEqual($minSpeed, $speed);
     }
 
@@ -102,5 +118,102 @@ class ProcessTest extends TestCase
             }
         }
         return array_sum($tables);
+    }
+
+    private $endpoints = [
+        ['l5-swagger.oauth2_callback', []],
+        ['horizon.stats.index', []],
+        ['horizon.workload.index', []],
+        ['horizon.masters.index', []],
+        ['horizon.monitoring.index', []],
+        ['horizon.jobs-metrics.index', []],
+        ['horizon.queues-metrics.index', []],
+        ['horizon.recent-jobs.index', []],
+        ['horizon.failed-jobs.index', []],
+        ['passport.authorizations.authorize', []],
+        ['passport.tokens.index', []],
+        ['passport.clients.index', []],
+        ['api.users.index', []],
+        ['api.groups.index', []],
+        ['api.group_members.index', []],
+        ['api.group_members_available.show', []],
+        ['api.user_members_available.show', []],
+        ['api.environment_variables.index', []],
+        ['api.screens.index', []],
+        ['api.screen_categories.index', []],
+        ['api.scripts.index', []],
+        ['api.processes.index', []],
+        ['api.processes.start', []],
+        ['api.process_categories.index', []],
+        ['api.tasks.index', []],
+        ['api.requests.index', []],
+        ['api.files.index', []],
+        ['api.notifications.index', []],
+        ['api.task_assignments.index', []],
+        ['api.comments.index', []],
+        ['groups.index', []],
+        ['users.index', []],
+        ['auth-clients.index', []],
+        ['customize-ui.edit', []],
+        ['admin.index', []],
+        ['environment-variables.index', []],
+        ['screens.index', []],
+        ['screens.import', []],
+        ['scripts.index', []],
+        ['categories.index', []],
+        ['processes.index', []],
+        ['processes.import', []],
+        ['processes.create', []],
+        ['about.index', []],
+        ['profile.edit', []],
+        ['home', []],
+        ['requests.index', []],
+        ['tasks.index', []],
+        ['notifications.index', []],
+        ['login', []],
+        ['logout', []],
+        ['password.request', []],
+        ['password-success', []],
+        ['error.unavailable', []],
+    ];
+
+    public function RoutesListProvider()
+    {
+        try {
+            $this->user = factory(User::class)->create(['is_administrator' => true]);
+
+            factory(Comment::class, 200)->create();
+        } catch (\Throwable $t) {
+            dump($t->getMessage());
+        }
+        return $this->endpoints;
+    }
+
+    /**
+     * Test routes speed
+     *
+     * @dataProvider RoutesListProvider
+     */
+    public function testRoutesSpeed($route, $params)
+    {
+        $this->actingAs($this->user);
+        $this->withoutExceptionHandling();
+
+        $baseTime = $this->calculateUnitTime();
+
+        // Test endpoint
+        $path = route($route, $params);
+        $fn = (substr($route, 0, 4) === 'api.') ? 'apiCall' : 'webCall';
+        $times = 10;
+        $t = microtime(true);
+        for ($i = 0;$i < $times;$i++) {
+            $this->$fn('GET', $path);
+        }
+        $time = microtime(true) - $t;
+
+        $requestsPerSecond = round($times / $time * 10) / 10;
+        $speed = $times / ($time / $baseTime);
+        echo "[$route = $speed]\n";
+        $this->assertGreaterThanOrEqual(3, $speed, "Slow route response [$route]\n             Speed ~ $requestsPerSecond [reqs/sec]");
     }
 }
