cbe04f162bb368a808b4d7d45ab46f587521f627
David Callizaya <davidcallizaya@gmail.com>
2020-02-06 12:27:10 -0400

    Calculate flow paths

--- tests/Feature/ProcessSimulationTest.php
+++ tests/Feature/ProcessSimulationTest.php
@@ -4,11 +4,11 @@ namespace Tests\Feature;
 
 use Exception;
 use ProcessMaker\Nayra\Contracts\Bpmn\ActivityInterface;
+use ProcessMaker\Nayra\Contracts\Bpmn\ProcessInterface;
 use ProcessMaker\Nayra\Contracts\Bpmn\StartEventInterface;
 use ProcessMaker\Nayra\Contracts\Engine\EngineInterface;
 use ProcessMaker\Nayra\Contracts\RepositoryInterface;
 use ProcessMaker\Nayra\Storage\BpmnDocument;
-use ProcessMaker\Simulator\Request;
 use Tests\Feature\Shared\ProcessTestingTrait;
 use Tests\Feature\Shared\RequestHelper;
 use Tests\TestCase;
@@ -33,6 +33,8 @@ class ProcessSimulationTest extends TestCase
      */
     private $engine;
 
+    private $variables = [];
+
     public function setupProcessCheckTest()
     {
         $this->bpmnRepository = app('workflow.test');
@@ -47,22 +49,63 @@ class ProcessSimulationTest extends TestCase
      */
     public function testProcessSimulation()
     {
-        $this->bpmnRepository->load(__DIR__ . '/Api/processes/ManualTask.bpmn');
+        $this->bpmnRepository->load(__DIR__ . '/Api/processes/ExclusiveGateway.bpmn');
+        $totalPaths = $this->calculatePaths();
+
         $processes = $this->bpmnRepository->getElementsByTagNameNS(BpmnDocument::BPMN_MODEL, 'process');
         foreach ($processes as $process) {
-            $dataStore = $this->factory->createDataStore();
-            $instance = $this->engine->createExecutionInstance($process->getBpmnElementInstance(), $dataStore);
             $startEvents = $process->getElementsByTagNameNS(BpmnDocument::BPMN_MODEL, 'startEvent');
             foreach ($startEvents as $startEvent) {
                 $start = $startEvent->getBpmnElementInstance();
-                $this->runInstance($instance, $start);
+                for ($seed = 0; $seed < $totalPaths; $seed++) {
+                    $this->runSimulation($process->getBpmnElementInstance(), $start, $seed);
+                }
             }
         }
     }
 
-    private function runInstance(Request $instance, StartEventInterface $start)
+    private function calculatePaths()
     {
-        $processes = $this->bpmnRepository->getElementsByTagNameNS(BpmnDocument::BPMN_MODEL, 'process');
+        $gateways = $this->bpmnRepository->getElementsByTagNameNS(BpmnDocument::BPMN_MODEL, 'exclusiveGateway');
+        $this->calculateGatewayPaths($gateways);
+        $gateways = $this->bpmnRepository->getElementsByTagNameNS(BpmnDocument::BPMN_MODEL, 'inclusiveGateway');
+        $this->calculateGatewayPaths($gateways);
+        return pow(2, count($this->variables));
+    }
+
+    private function calculateGatewayPaths($gateways)
+    {
+        foreach ($gateways as $gateway) {
+            $outgoings = $gateway->getElementsByTagNameNS(BpmnDocument::BPMN_MODEL, 'outgoing');
+            $default = $gateway->getAttribute('default');
+            foreach ($outgoings as $outgoing) {
+                if ($outgoing->nodeValue !== $default) {
+                    $flow = $this->bpmnRepository->findElementById($outgoing->nodeValue);
+                    $condition = $flow->getElementsByTagNameNS(BpmnDocument::BPMN_MODEL, 'conditionExpression')->item(0);
+                    if ($condition) {
+                        $condition->nodeValue = $this->addPathVariable();
+                    }
+                }
+            }
+        }
+    }
+
+    private function addPathVariable()
+    {
+        $variable = 'v' . count($this->variables);
+        $this->variables[] = $variable;
+        return $variable;
+    }
+
+    private function runSimulation(ProcessInterface $process, StartEventInterface $start, $seed = 0)
+    {
+        $dataStore = $this->factory->createDataStore();
+        $data = [];
+        foreach ($this->variables as $i => $variable) {
+            $data[$variable] = boolval(($seed >> $i) & 1);
+        }
+        $dataStore->setData($data);
+        $instance = $this->engine->createExecutionInstance($process, $dataStore);
         $start->start($instance);
         $this->engine->runToNextState();
         $tokens = $instance->getTokens();
